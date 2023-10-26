cbe667ea7a8c80acb1a0a8a2ccfbcbb28a72b4b6
Amit Kumar <thisiskumaramit@gmail.com>
2023-10-26 14:44:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const[colar, setcolor]=useState("burlywood");
+
+  const whiteSmoke=()=>{
+    setcolor("whiteSmoke");
+  }
+
+  const darkcyan=()=>{
+    setcolor("darkcyan");
+  }
+
+  const darkorange=()=>{
+    setcolor("darkorange");
+  }
+
+  const gainsboro=()=>{
+    setcolor("gainsboro");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background:colar, height:"450px", borderRadius:"60px"}}>
+    <h1 style={{color:"brown"}}>Bg-Changer
+    <p>It is background color changer, it takes only one click to change background color.</p></h1>
+      <div style={{margin:"100px 400px",padding:"230px 60px",display:"flex",flexWrap:"wrap", alignItems:"center"}}>
+          <div style={{padding:"10px 2px"}}>
+            <button onClick={whiteSmoke} style={{borderRadius:"30px", padding:"5px 5px"}}>whiteSmoke</button>
+          </div>
+          <div style={{padding:"10px 2px"}}>
+            <button onClick={darkcyan} style={{borderRadius:"30px", padding:"4px 5px"}}>darkcyan</button>
+          </div>
+          <div style={{padding:"10px 2px"}}>
+            <button onClick={darkorange} style={{borderRadius:"30px", padding:"4px 5px"}}>darkorange</button>
+          </div>
+          <div style={{padding:"10px 2px"}}>
+            <button onClick={gainsboro} style={{borderRadius:"30px", padding:"4px 5px"}}>gainsboro</button>
+          </div>
+      </div>
     </div>
   );
 }
